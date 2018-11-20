f3296cb05c097c814a88cd523a67fc7d3d8073e8
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-11-20 17:44:10 +0100

    hop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+100.times do
+  Article.create(
+    title: Faker::ElderScrolls.creature,
+    content: Faker::ChuckNorris.fact
+    )
+end
